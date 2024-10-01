6b38750c430061a379884eb3ecd90c2845bac688
Selvakumar SK <92006061+Selva-kumar-K@users.noreply.github.com>
2024-10-01 22:09:38 +0530

    Updated blogdata.ts

--- src/utils/blogdata.ts
+++ src/utils/blogdata.ts
@@ -1,16 +1,16 @@
 export const blogData = [
   {
-    userImg: "src/assets/blog1.png",
+    userImg: "assets/blog1.png",
     username: "Selvakumar",
     content: "Soil testing made easy to farm now-a-days.",
   },
   {
-    userImg: "src/assets/blog2.png",
+    userImg: "assets/blog2.png",
     username: "Nithish",
     content: "Soil testing made easy to farm now-a-days.",
   },
   {
-    userImg: "src/assets/blog3.png",
+    userImg: "assets/blog3.png",
     username: "Anirudh",
     content: "Soil testing made easy to farm now-a-days.",
   },
