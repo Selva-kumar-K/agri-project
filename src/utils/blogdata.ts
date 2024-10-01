2735276675139fd9fa1e0abe08f25f4e19019f63
Selvakumar SK <92006061+Selva-kumar-K@users.noreply.github.com>
2024-10-01 22:15:36 +0530

    Updated image path in blogdata.ts

--- src/utils/blogdata.ts
+++ src/utils/blogdata.ts
@@ -1,16 +1,20 @@
+import blog1 from "../assets/blog1.png";
+import blog2 from "../assets/blog2.png";
+import blog3 from "../assets/blog3.png";
+
 export const blogData = [
   {
-    userImg: "assets/blog1.png",
+    userImg: blog1,
     username: "Selvakumar",
     content: "Soil testing made easy to farm now-a-days.",
   },
   {
-    userImg: "assets/blog2.png",
+    userImg: blog2,
     username: "Nithish",
     content: "Soil testing made easy to farm now-a-days.",
   },
   {
-    userImg: "assets/blog3.png",
+    userImg: blog3,
     username: "Anirudh",
     content: "Soil testing made easy to farm now-a-days.",
   },
